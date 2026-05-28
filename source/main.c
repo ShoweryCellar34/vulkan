aa8e42a77cc1f5c332c9218e61536f989aa9d844
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2026-05-29 09:13:44 +1200

    setup cmake

--- source/main.c
+++ source/main.c
@@ -5,7 +5,7 @@
 #include <errno.h>
 
 // Library Headers
-
+#include <vulkan/vulkan.h>
 
 // Project Headers
 #include <projectData.h>
@@ -18,37 +18,6 @@ int main(int argc, char* argv[]) {
     for(int i = 0; i < argc; i++) {
         printf("Arg %i:           %s\n", i, argv[i]);
     }
-
-    // Define resource file
-    const char* resourceFilePath = "resources/resource.txt";
-
-    // Open resource file
-    FILE* filePointer = fopen(resourceFilePath, "r");
-    if(!filePointer) {
-        fprintf(stderr, "Failed to open %s: %s\n", resourceFilePath, strerror(errno));
-        return EXIT_FAILURE;
-    }
-    printf("Opened %s\n-----------------------------\n", resourceFilePath);
-
-    // Define buffer to store resource file contents
-    char stringBuffer[128];
-
-    // Clear error code and read resource file line by line
-    errno = 0;
-    while(fgets(stringBuffer, sizeof(stringBuffer), filePointer)) {
-        printf("%s", stringBuffer);
-    }
-
-    // Check for reading errors
-    if(errno) {
-        fprintf(stderr, "Failed to read %s: %s\n", resourceFilePath, strerror(errno));
-        fclose(filePointer);
-        return EXIT_FAILURE;
-    }
-
-    // Close resource file
-    fclose(filePointer);
-    printf("-----------------------------\nClosed %s\n", resourceFilePath);
 
     // Exit with success
     return EXIT_SUCCESS;
