7ca869a090232d8da2469c589e3757d195152c68
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2026-06-04 23:08:04 +1200

    introduce volk and random error

--- source/main.c
+++ source/main.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+// #include <errno.h>
 
 // Library Headers
-#include <vulkan/vulkan.h>
+#include <volk.h>
+#include <GLFW/glfw3.h>
 
 // Project Headers
 #include <projectData.h>
+
+#define WINDOW_WIDTH  800
+#define WINDOW_HEIGHT 450
 
 int main(int argc, char* argv[]) {
     // Output project info and argument count
@@ -18,6 +22,27 @@ int main(int argc, char* argv[]) {
     for(int i = 0; i < argc; i++) {
         printf("Arg %i:           %s\n", i, argv[i]);
     }
+
+    // initialize GLFW and check for vulkan support, exit if initialization fails or vulkan not supported
+    if(!glfwInit() || !glfwVulkanSupported()) {
+        return EXIT_FAILURE;
+    }
+
+    // Disable the creation of an OpenGL Context and disable window resizing
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    // Create the window
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, PROJECT_NAME, NULL, NULL);
+
+    // Main application loop
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    // Cleanup GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     // Exit with success
     return EXIT_SUCCESS;
