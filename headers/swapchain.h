b2370ee9174aae42a73af05987846f9d18688d32
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2026-07-22 10:12:52 +1200

    idk2

--- headers/swapchain.h
+++ headers/swapchain.h
@@ -7,13 +7,19 @@
 #include <volk.h>
 
 // Project Headers
+#include <devices.h>
 #include <macros.h>
 
 typedef struct {
+    PhysicalDeviceCreateInfo physicalDeviceCreateInfo;
+
     VkSwapchainKHR swapchain;
     VkExtent2D     swapchainExtent;
     ImageSlice     swapchainImages;
     ImageViewSlice swapchainImageViews;
 } SwapchainInfo;
+
+// Creates a vulkan swapchain
+void CreateVulkanSwapchain(SwapchainInfo* swapchainInfo, VkExtent2D desiredExtent);
 
 #endif // SWAPCHAIN_H
